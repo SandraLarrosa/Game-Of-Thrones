649ddfe3a13def19a30df77cfcc26d5f8506ba7c
Alexandra <sandra.larrosa88@gmail.com>
2020-08-01 12:06:19 +0200

    run docs

---
+++ docs/precache-manifest.815014065d664cffc8446aa01e57207e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29da7fdf7c22fa9ca385e76ed824672",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b84088aeaee8f83fdd2",
+    "url": "./static/css/main.65a53575.chunk.css"
+  },
+  {
+    "revision": "9044f0ad07e9c890674a",
+    "url": "./static/js/2.ba45bc4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ba45bc4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b84088aeaee8f83fdd2",
+    "url": "./static/js/main.a03cd4eb.chunk.js"
+  },
+  {
+    "revision": "b388ac9c340cc6e2817c",
+    "url": "./static/js/runtime-main.fa365cae.js"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "./static/media/Game-of-Thrones.3af178ec.ttf"
+  }
+]);
